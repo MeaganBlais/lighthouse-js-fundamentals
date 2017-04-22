456a6e197c3aac79ec56d113f51859336ce1da37
Meagan Blais <meaganblais@gmail.com>
2017-04-22 16:31:40 -0400

    Adds conditionals exercises plus additional work

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return(name + " is " + (currentYear - yearOfBirth) + " years old.");
+}
+
+ageCalculator("Miranda", 1983, 2015);
+ageCalculator("Ferdinand", 1988, 2015);
